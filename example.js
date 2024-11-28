787d42ce0f2aca0e3b84bca24f7b11d55a534449
Thon <137491012+thon1525@users.noreply.github.com>
2024-11-28 16:49:18 +0000

    add this code

---
+++ example.js
@@ -0,0 +1,20 @@
+const data ={
+    name: "thon",
+    type: "f",
+}
+const data_Provinde= [
+    'takov',
+    'pp',
+    'pring vong'
+
+]
+const object_data = {
+  name: "thon",
+  type: "M",
+  price: 333,
+}
+const {name, type, price} = object_data;
+const rm= data_Provinde.push("kalnel")
+console.log(rm)
+console.log(data_Provinde)
+console.log(data.name)
